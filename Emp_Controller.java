a97299905c79cc03ee895198b1e89bf86856882b
Mareeswaran <118057031+Mareeswaran27@users.noreply.github.com>
2025-04-27 21:37:09 +0530

    Update Emp_Controller.java

--- Emp_Controller.java
+++ Emp_Controller.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.repository.Employee_Repositary; 
+import com.example.demo.repository.Employee_Repository; 
 import com.example.demo.model.employees;
 
 @RestController
