2648131cf4136db485c8396d43480f7c7745f0d6
Mareeswaran <118057031+Mareeswaran27@users.noreply.github.com>
2025-04-27 21:35:33 +0530

    Update and rename Employee_Repositary.java to Employee_Repository.java

--- Employee_Repository.java
+++ Employee_Repository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Employee_Repositary extends MongoRepository<employees, String> {
+public interface Employee_Repository extends MongoRepository<employees, String> {
 }
